540c54db45d1e0192290f4c3037ff5662e7510ae
skyrafael <skyrafael@hotmail.com>
2018-06-20 23:23:55 -0300

    inicial commit

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,15 @@ import { CommonModule, } from '@angular/common';
 import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {EquipeComponent} from './equipe/equipe.component';
+import {ContatoComponent} from './contato/contato.component';
 
 
 const routes: Routes = [
     /*pseu*/
     { path: 'home',         component: HomeComponent },
+    { path: 'equipe',         component: EquipeComponent },
+    { path: 'contato',         component: ContatoComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
