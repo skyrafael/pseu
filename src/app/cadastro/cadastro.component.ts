183734106598c2138528a2ef086ba6ff468cd9b7
skyrafael <skyrafael@hotmail.com>
2018-06-26 01:29:38 -0300

    continuando

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
+  data : Date = new Date();
+
   constructor() { }
 
   ngOnInit() {
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.add('full-screen');
+    body.classList.add('register');
+    var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.add('navbar-transparent');
+  }
+  ngOnDestroy(){
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.remove('full-screen');
+    body.classList.remove('register');
+    var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.remove('navbar-transparent');
   }
 
 }
